27ec741224582621ac74ee2ed5141ad5408d6ccf
John Eargle <jeargle@gmail.com>
2016-10-19 23:29:08 -0500

    added base set of game states

--- js/spacemo.js
+++ js/spacemo.js
@@ -1,25 +1,68 @@
-var jMain, score, game;
+var bootState, loadState, titleState, playState, endState, score, game;
 
 score = 0;
 
 
-jMain = {
+bootState = {
+    create: function() {
+        'use strict';
+
+        // Load physics engine
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.state.start('load');
+    }
+};
+
+loadState = {
     preload: function() {
         'use strict';
+
+        // Load art assets
     },
+    create: function() {
+        'use strict';
+        game.state.start('title');
+    }
+};
+
+titleState = {
+    create: function() {
+        'use strict';
+    },
+    start: function() {
+        'use strict';
+        game.state.start('play');
+    }
+};
+
+playState = {
     create: function() {
         'use strict';
     },
     update: function() {
         'use strict';
+    },
+    end: function() {
+        'use strict';
+        game.state.start('play');
     }
-}
+};
+
+endState = {
+    create: function() {
+        'use strict';
+    },
+    restart: function() {
+        'use strict';
+        game.state.start('title');
+    }
+};
 
 
-game = new Phaser.Game(
-    800, 600,
-    Phaser.AUTO, '',
-    {preload: jMain.preload,
-     create: jMain.create,
-     update: jMain.update}
-);
+game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-div');
+
+game.state.add.('boot', bootState);
+game.state.add.('load', loadState);
+game.state.add.('title', titleState);
+game.state.add.('play', playState);
+game.state.add.('end', endState);
