5826245ddfcde7ddf81c53c225ff1c63bae757f4
John Eargle <jeargle@gmail.com>
2016-10-22 18:15:31 -0500

    full set of working states

--- js/spacemo.js
+++ js/spacemo.js
@@ -19,7 +19,8 @@ loadState = {
         var loadLbl;
 
         loadLbl = game.add.text(80, 160, 'loading...',
-                                {font: '30px Courier', fill: '#ffffff'});
+                                {font: '30px Courier',
+                                 fill: '#ffffff'});
         
         // Load art assets
         game.load.image('player', 'assets/square-red.png');
@@ -37,9 +38,11 @@ titleState = {
         var nameLbl, startLbl, wKey;
 
         nameLbl = game.add.text(80, 160, 'SPACEMO',
-                                {font: '50px Courier', fill: '#ffffff'});
+                                {font: '50px Courier',
+                                 fill: '#ffffff'});
         startLbl = game.add.text(80, 240, 'press "W" to start',
-                                 {font: '30px Courier', fill: '#ffffff'});
+                                 {font: '30px Courier',
+                                  fill: '#ffffff'});
 
         wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
         wKey.onDown.addOnce(this.start, this);
@@ -53,19 +56,60 @@ titleState = {
 playState = {
     create: function() {
         'use strict';
+
+        this.keyboard = game.input.keyboard;
+
+        this.player = game.add.sprite(16, 16, 'player');
+        game.physics.enable(this.player, Phaser.Physics.ARCADE);
+
+        this.exit = game.add.sprite(256, 256, 'exit');
+        game.physics.enable(this.exit, Phaser.Physics.ARCADE);
     },
     update: function() {
         'use strict';
+
+        game.physics.arcade.overlap(this.player, this.exit, this.end, null, this);
+
+        if (this.keyboard.isDown(Phaser.Keyboard.A)) {
+            this.player.body.velocity.x = -175;
+        }
+        else if (this.keyboard.isDown(Phaser.Keyboard.D)) {
+            this.player.body.velocity.x = 175;
+        }
+        else {
+            this.player.body.velocity.x = 0;
+        }
+
+        if (this.keyboard.isDown(Phaser.Keyboard.W)) {
+            this.player.body.velocity.y = -175;
+        }
+        else if (this.keyboard.isDown(Phaser.Keyboard.S)) {
+            this.player.body.velocity.y = 175;
+        }
+        else {
+            this.player.body.velocity.y = 0;
+        }
     },
     end: function() {
         'use strict';
-        game.state.start('play');
+        game.state.start('end');
     }
 };
 
 endState = {
     create: function() {
         'use strict';
+        var nameLbl, startLbl, wKey;
+
+        nameLbl = game.add.text(80, 160, 'YOU WON',
+                                {font: '50px Courier',
+                                 fill: '#ffffff'});
+        startLbl = game.add.text(80, 240, 'press "W" to restart',
+                                 {font: '30px Courier',
+                                  fill: '#ffffff'});
+
+        wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+        wKey.onDown.addOnce(this.restart, this);
     },
     restart: function() {
         'use strict';
